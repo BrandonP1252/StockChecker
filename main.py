51dd6a80b3a29081184b9152f0ac661bb519f0f5
BrandonP1252 <przbrandon1212@gmail.com>
2023-01-18 14:50:44 -0500

    Added functions to check element and check stock

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
-import useragent
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-options = Options()
-user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
-options.add_argument(f'user-agent={user_agent}')
-options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get(
-    "https://www.bestbuy.com/site/lenovo-ideapad-3i-15-6-hd-touch-laptop-core-i3-1115g4-8gb-memory-256gb-ssd-platinum-grey/6511950.p?skuId=6511950")
-print("got here")
-try:
-    element = driver.find_element(By.XPATH, '//button[text()="Add to Cart"]')
-    print("In Stock")
-except:
-    print("Sold out")
+def setup(user_input):
+    options = Options()
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    print("Type 'Stop' to stop checking")
+    check_stock(user_input, driver)
 
+
+def check_stock(string, driver):
+    while True:
+        driver.get(string)
+        if check_element('//button[text()="Add to Cart"]', driver):
+            print("In Stock")
+            break
+            # send email
+        else:
+            print("Out of Stock")
+        time.sleep(3)
+
+
+def check_element(string, driver):
+    try:
+        element = driver.find_element(By.XPATH, string)
+        return True
+    except:
+        return False
+
+
+def main():
+    user_input = input("Enter bestbuy URL: ")
+    print("Running...")
+    setup(user_input)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
